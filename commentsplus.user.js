b09c9c273fb942f5c5c4ae77489dd405e2a64e5e
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2018-07-23 23:33:06 -0700

    Rewrite comment indexes and quote links

---
+++ commentsplus.user.js
@@ -0,0 +1,104 @@
+// ==UserScript==
+// @name           Fimfiction Comments Plus
+// @description    Enhanced comments for Fimfiction
+// @author         Pluie
+// @version        0.0.1
+// @homepageURL    https://github.com/PluieElectrique/fimfic-comments-plus
+// @downloadURL    https://github.com/PluieElectrique/fimfic-comments-plus/raw/master/commentsplus.user.js
+// @updateURL      https://github.com/PluieElectrique/fimfic-comments-plus/raw/master/commentsplus.user.js
+// @match          *://www.fimfiction.net/*
+// @grant          unsafeWindow
+// @run-at         document-end
+// ==/UserScript==
+
+let App = unsafeWindow.App;
+
+let smuggle = f => {
+    if (typeof exportFunction !== "function") {
+        return f;
+    } else {
+        // Firefox requires us to export any functions which will be called by code in the page's
+        // scope (this includes functions passed as callbacks). Functions which are called by
+        // exported functions do not have to be exported.
+        return exportFunction(f, window);
+    }
+};
+
+let pagesStored = [];
+let comments = {};
+
+let rewriteQuoteLinks = elem => {
+    for (let quoteLink of elem.querySelectorAll(".comment_quote_link:not(.comment_callback)")) {
+        let meta = comments[quoteLink.dataset.comment_id];
+        if (meta !== undefined) {
+            quoteLink.textContent = meta.author;
+        }
+    }
+};
+
+// A collection of methods that will be assigned onto the real comment controller
+let commentControllerShell = {
+    getComment: smuggle(function(id) {
+        let comment = document.getElementById("comment_" + id);
+        if (comment !== null) {
+            return new Promise(f => f(comment));
+        }
+
+        return Object.getPrototypeOf(this).getComment
+            .call(this, id)
+            .then(smuggle(comment => {
+                let meta = comments[id];
+                if (meta !== undefined) {
+                    // Rewrite comment index
+                    comment.querySelector(`[href='#comment/${id}']`).textContent = "#" + meta.index;
+                }
+                rewriteQuoteLinks(comment);
+                return comment;
+            }));
+    }),
+
+    setupQuotes: smuggle(function() {
+        Object.getPrototypeOf(this).setupQuotes.call(this);
+        rewriteQuoteLinks(document);
+        this.storeComments();
+    }),
+
+    goToPage: smuggle(function(num) {
+        this.storeComments();
+        Object.getPrototypeOf(this).goToPage.call(this, num);
+    }),
+
+    // Extra methods for ease of accessing `this`
+    storeComments: function() {
+        if (pagesStored[this.currentPage]) return;
+        pagesStored[this.currentPage] = true;
+
+        let ordering, startIndex;
+        if (this.order === "ASC") {
+            ordering = 1;
+            startIndex = Number(document.querySelector(".start-index").textContent);
+        } else {
+            ordering = -1;
+            // In ASC ordering, .end-index is incorrectly rounded up to the nearest multiple of 50.
+            // But it is correct in DESC ordering, so we can use it here.
+            // TODO: fix this
+            startIndex = Number(document.querySelector(".end-index").textContent);
+        }
+
+        document.querySelectorAll(".comment").forEach((comment, i) => {
+            // Is this a deleted comment?
+            if (comment.childElementCount === 2) return;
+
+            comments[comment.dataset.comment_id] = {
+                author: comment.dataset.author,
+                index: startIndex + ordering * i
+            };
+        });
+    }
+};
+
+let storyComments = document.getElementById("story_comments");
+if (storyComments !== null) {
+    let commentController = App.createdControllers[storyComments.dataset.controllerId];
+    Object.assign(commentController, commentControllerShell);
+}
