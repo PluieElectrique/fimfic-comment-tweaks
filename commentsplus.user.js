c5d8503ec01beefa11e2d48b18025508394b8f2d
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2019-02-24 03:36:26 -0800

    Reorganize code

--- commentsplus.user.js
+++ commentsplus.user.js
@@ -10,118 +10,89 @@
 // @run-at         document-idle
 // ==/UserScript==
 
-function createMiddot() {
-    let middot = document.createElement("b");
-    middot.textContent = "\u00b7";
-    return middot;
+// Despite the @run-at option, Firefox sometimes runs the userscript before the Fimfiction JS, which
+// causes errors. So, we wait for the page to be fully loaded.
+if (document.readyState == "complete") {
+    init();
+} else {
+    window.addEventListener("load", init);
 }
 
-// Clone a comment unhidden and without expanded links and collapse button/middot
-function cloneComment(comment) {
-    // Remove quotes to avoid cloning them
-    let commentCallbacks = comment.querySelector(".comment_callbacks");
-    let callbackQuotes = commentCallbacks.querySelectorAll(".inline-quote");
-    for (let quote of callbackQuotes) {
-        fQuery.removeElement(quote);
+let cplusCSS = `
+.cplus--collapse-button { padding: 3px; }
+.cplus--collapse-button:not(:hover) { opacity: 0.7; }
+@media all and (min-width: 701px) { .inline-quote .meta > .name { display: inline; } }
+.comment .data { padding-right: 0.3rem; }
+.comment.cplus--forward-hidden { display: none; }
+.comment.cplus--collapsed .author > .avatar { display: none; }
+.comment.cplus--collapsed .comment_callbacks > a { opacity: 0.7; }
+.comment.cplus--collapsed .comment_callbacks > div { display: none; }
+.comment.cplus--collapsed .comment_data { display: none; }
+.comment.cplus--collapsed .comment_information:after { height: 0; }
+.cplus--expanded-link { opacity: 0.7; }
+.cplus--parent-link { text-decoration: underline; }
+`;
+
+function init() {
+    let storyComments = document.getElementById("story_comments");
+    if (storyComments === null) {
+        return;
     }
 
-    let commentData = comment.querySelector(".comment_data");
-    let dataQuotes = [];
-    for (let quote of commentData.querySelectorAll(".inline-quote")) {
-        // Get the link first. If we remove the quote first, then the sibling will be null.
-        dataQuotes.push({
-            link: quote.previousElementSibling,
-            quote: quote.parentNode.removeChild(quote)
-        });
-    }
+    let style = document.createElement("style");
+    style.type = "text/css";
+    style.textContent = cplusCSS;
+    document.head.appendChild(style);
 
-    let clone = comment.cloneNode(true);
-    clone.removeAttribute("id");
-    clone.classList.remove("cplus--forward-hidden");
-    clone.classList.remove("cplus--collapsed");
+    let commentController = App.GetControllerFromElement(storyComments);
+    Object.assign(commentController, commentControllerShell);
 
-    // Remove middot and collapse button
-    let collapseButton = clone.querySelector(".cplus--collapse-button");
-    if (collapseButton !== null) {
-        fQuery.removeElement(collapseButton.nextElementSibling);
-        fQuery.removeElement(collapseButton);
-    }
+    setupCollapseButtons();
 
-    // Restore quotes
-    for (let quote of callbackQuotes) {
-        commentCallbacks.appendChild(quote);
-    }
+    fQuery.addScopedEventListener(
+        commentController.comment_list,
+        ".cplus--collapse-button",
+        "click",
+        evt => toggleCollapseCommentTree(fQuery.closestParent(evt.target, ".comment"))
+    );
 
-    for (let quote of dataQuotes) {
-        fQuery.insertAfter(quote.link, quote.quote);
-    }
+    fQuery.addScopedEventListener(
+        commentController.comment_list,
+        ".comment_quote_link",
+        "mouseover",
+        evt => {
+            // Remove 150ms delay by preventing the normal event listener from firing
+            evt.stopPropagation();
+            // Don't show popup quote for expanded links, links of collapsed comments, or links to
+            // the parent comment
+            let isExpanded = evt.target.classList.contains("cplus--expanded-link");
+            let isCollapsed = fQuery
+                .closestParent(evt.target, ".comment")
+                .classList.contains("cplus--collapsed");
+            let isParentLink = evt.target.classList.contains("cplus--parent-link");
+            if (!isExpanded && !isCollapsed && !isParentLink) {
+                commentController.beginShowQuote(evt.target);
+            }
+        }
+    );
 
-    return clone;
-}
-
-// Mark the quote link to the parent as a visual cue and to prevent infinite nesting
-function markParentLink(parent, child) {
-    let parentId = parent.dataset.comment_id;
-    let childLink = child.querySelector(`.comment_quote_link[data-comment_id='${parentId}']`);
-    if (childLink !== null) {
-        childLink.classList.add("cplus--parent-link");
-    }
-}
-
-// If the link is a callback, update the expansion count of its comment and hide/unhide if needed.
-function forwardHide(quoteLink, change) {
-    if (!quoteLink.parentElement.classList.contains("comment_callbacks")) return;
-    if (change !== 1 && change !== -1) {
-        throw new Error("Change to expand count must be 1 or -1");
-    }
-
-    let comment = document.getElementById("comment_" + quoteLink.dataset.comment_id);
-    let newCount = Number(comment.dataset.expandCount || 0) + change;
-    if (newCount < 0) {
-        throw new Error("Expand count cannot be less than 0");
-    } else if (newCount === 0) {
-        comment.classList.remove("cplus--forward-hidden");
-    } else if (newCount === 1) {
-        comment.classList.add("cplus--forward-hidden");
-    }
-    comment.dataset.expandCount = newCount;
-}
-
-function setupCollapseButtons() {
-    for (let metaName of document.querySelectorAll(".meta > .name")) {
-        fQuery.insertAfter(metaName, createMiddot());
-
-        let collapseButton = document.createElement("a");
-        collapseButton.classList.add("cplus--collapse-button");
-        let minus = document.createElement("i");
-        minus.classList.add("fa", "fa-minus-square-o");
-        collapseButton.appendChild(minus);
-        fQuery.insertAfter(metaName, collapseButton);
+    // quote_container is used by beginShowQuote to store the hovered quote (when there is one). In
+    // the original code, it's checked for on each call. Here, we create it at init.
+    if (commentController.quote_container === null) {
+        let container = document.createElement("div");
+        container.className = "quote_container";
+        document.body.appendChild(container);
+        commentController.quote_container = container;
     }
 }
 
-function toggleCollapseCommentTree(comment) {
-    collapseCommentTree(comment, !comment.classList.contains("cplus--collapsed"));
-}
-function collapseCommentTree(comment, collapse) {
-    comment.classList.toggle("cplus--collapsed", collapse);
-
-    let collapseIcon = comment.querySelector(".cplus--collapse-button > i");
-    collapseIcon.classList.toggle("fa-plus-square-o", collapse);
-    collapseIcon.classList.toggle("fa-minus-square-o", !collapse);
-
-    for (let callback of comment.querySelectorAll(".comment_callback")) {
-        let id = "comment_" + callback.dataset.comment_id;
-        collapseCommentTree(document.getElementById(id), collapse);
-    }
-}
-
-// An object that will be assigned onto the real comment controller
+// An wrapper object that will be assigned onto the real comment controller
 let commentControllerShell = {
     // Map from comment number (`data-comment_id`) to { author, index }
     commentMetadata: {},
 
-    // Methods that shadow existing methods
+    /* Methods that shadow existing methods */
+
     getComment: function(id) {
         let comment = document.getElementById("comment_" + id);
         if (comment !== null) {
@@ -239,7 +210,8 @@ let commentControllerShell = {
         }
     },
 
-    // Extra methods for ease of accessing `this`
+    /* Extra methods for ease of accessing `this` */
+
     storeComments: function() {
         let indexToNumber = indexClass =>
             Number(document.querySelector(indexClass).textContent.replace(/,/g, ""));
@@ -288,76 +260,108 @@ let commentControllerShell = {
     }
 };
 
-let cssCode = `
-.cplus--collapse-button { padding: 3px; }
-.cplus--collapse-button:not(:hover) { opacity: 0.7; }
-@media all and (min-width: 701px) { .inline-quote .meta > .name { display: inline; } }
-.comment .data { padding-right: 0.3rem; }
-.comment.cplus--forward-hidden { display: none; }
-.comment.cplus--collapsed .author > .avatar { display: none; }
-.comment.cplus--collapsed .comment_callbacks > a { opacity: 0.7; }
-.comment.cplus--collapsed .comment_callbacks > div { display: none; }
-.comment.cplus--collapsed .comment_data { display: none; }
-.comment.cplus--collapsed .comment_information:after { height: 0; }
-.cplus--expanded-link { opacity: 0.7; }
-.cplus--parent-link { text-decoration: underline; }
-`;
+function setupCollapseButtons() {
+    for (let metaName of document.querySelectorAll(".meta > .name")) {
+        fQuery.insertAfter(metaName, createMiddot());
 
-function init() {
-    let storyComments = document.getElementById("story_comments");
-    if (storyComments !== null) {
-        let commentController = App.GetControllerFromElement(storyComments);
-        Object.assign(commentController, commentControllerShell);
-
-        setupCollapseButtons();
-
-        fQuery.addScopedEventListener(
-            commentController.comment_list,
-            ".cplus--collapse-button",
-            "click",
-            evt => toggleCollapseCommentTree(fQuery.closestParent(evt.target, ".comment"))
-        );
-
-        fQuery.addScopedEventListener(
-            commentController.comment_list,
-            ".comment_quote_link",
-            "mouseover",
-            evt => {
-                // Remove 150ms delay by preventing the normal event listener from firing
-                evt.stopPropagation();
-                // Don't show popup quote for expanded links, links of collapsed comments, or links
-                // the parent comment
-                let isExpanded = evt.target.classList.contains("cplus--expanded-link");
-                let isCollapsed = fQuery
-                    .closestParent(evt.target, ".comment")
-                    .classList.contains("cplus--collapsed");
-                let isParentLink = evt.target.classList.contains("cplus--parent-link");
-                if (!isExpanded && !isCollapsed && !isParentLink) {
-                    commentController.beginShowQuote(evt.target);
-                }
-            }
-        );
-
-        let style = document.createElement("style");
-        style.type = "text/css";
-        style.textContent = cssCode;
-        document.head.appendChild(style);
-
-        // quote_container is used by beginShowQuote to store the hovered quote (when there is one).
-        // In the original code, it's checked for on each call. Here, we create it at init.
-        if (commentController.quote_container === null) {
-            let container = document.createElement("div");
-            container.className = "quote_container";
-            document.body.appendChild(container);
-            commentController.quote_container = container;
-        }
+        let collapseButton = document.createElement("a");
+        collapseButton.classList.add("cplus--collapse-button");
+        let minus = document.createElement("i");
+        minus.classList.add("fa", "fa-minus-square-o");
+        collapseButton.appendChild(minus);
+        fQuery.insertAfter(metaName, collapseButton);
     }
 }
 
-// Despite the @run-at option, Firefox sometimes runs the userscript before the Fimfiction JS, which
-// causes errors. So, we wait for the page to be fully loaded.
-if (document.readyState == "complete") {
-    init();
-} else {
-    window.addEventListener("load", init);
+// If the link is a callback, update the expansion count of its comment and hide/unhide if needed.
+function forwardHide(quoteLink, change) {
+    if (!quoteLink.parentElement.classList.contains("comment_callbacks")) return;
+    if (change !== 1 && change !== -1) {
+        throw new Error("Change to expand count must be 1 or -1");
+    }
+
+    let comment = document.getElementById("comment_" + quoteLink.dataset.comment_id);
+    let newCount = Number(comment.dataset.expandCount || 0) + change;
+    if (newCount < 0) {
+        throw new Error("Expand count cannot be less than 0");
+    } else if (newCount === 0) {
+        comment.classList.remove("cplus--forward-hidden");
+    } else if (newCount === 1) {
+        comment.classList.add("cplus--forward-hidden");
+    }
+    comment.dataset.expandCount = newCount;
+}
+
+function toggleCollapseCommentTree(comment) {
+    collapseCommentTree(comment, !comment.classList.contains("cplus--collapsed"));
+}
+function collapseCommentTree(comment, collapse) {
+    comment.classList.toggle("cplus--collapsed", collapse);
+
+    let collapseIcon = comment.querySelector(".cplus--collapse-button > i");
+    collapseIcon.classList.toggle("fa-plus-square-o", collapse);
+    collapseIcon.classList.toggle("fa-minus-square-o", !collapse);
+
+    for (let callback of comment.querySelectorAll(".comment_callback")) {
+        let id = "comment_" + callback.dataset.comment_id;
+        collapseCommentTree(document.getElementById(id), collapse);
+    }
+}
+
+// Clone a comment unhidden and without expanded links and collapse button/middot
+function cloneComment(comment) {
+    // Remove quotes to avoid cloning them
+    let commentCallbacks = comment.querySelector(".comment_callbacks");
+    let callbackQuotes = commentCallbacks.querySelectorAll(".inline-quote");
+    for (let quote of callbackQuotes) {
+        fQuery.removeElement(quote);
+    }
+
+    let commentData = comment.querySelector(".comment_data");
+    let dataQuotes = [];
+    for (let quote of commentData.querySelectorAll(".inline-quote")) {
+        // Get the link first. If we remove the quote first, then the sibling will be null.
+        dataQuotes.push({
+            link: quote.previousElementSibling,
+            quote: quote.parentNode.removeChild(quote)
+        });
+    }
+
+    let clone = comment.cloneNode(true);
+    clone.removeAttribute("id");
+    clone.classList.remove("cplus--forward-hidden");
+    clone.classList.remove("cplus--collapsed");
+
+    // Remove middot and collapse button
+    let collapseButton = clone.querySelector(".cplus--collapse-button");
+    if (collapseButton !== null) {
+        fQuery.removeElement(collapseButton.nextElementSibling);
+        fQuery.removeElement(collapseButton);
+    }
+
+    // Restore quotes
+    for (let quote of callbackQuotes) {
+        commentCallbacks.appendChild(quote);
+    }
+
+    for (let quote of dataQuotes) {
+        fQuery.insertAfter(quote.link, quote.quote);
+    }
+
+    return clone;
+}
+
+// Mark the quote link to the parent as a visual cue and to prevent infinite nesting
+function markParentLink(parent, child) {
+    let parentId = parent.dataset.comment_id;
+    let childLink = child.querySelector(`.comment_quote_link[data-comment_id='${parentId}']`);
+    if (childLink !== null) {
+        childLink.classList.add("cplus--parent-link");
+    }
+}
+
+function createMiddot() {
+    let middot = document.createElement("b");
+    middot.textContent = "\u00b7";
+    return middot;
 }
